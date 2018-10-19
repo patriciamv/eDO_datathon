c928dbfb8c997e1885a3fb29ad7d0cac18229661
Patricia Marquez <pmarquez@digitialorigin.com>
2018-10-19 22:05:54 +0200

    adaption for auto ML

--- code/04_feature_engineering.R
+++ code/04_feature_engineering.R
@@ -1,5 +1,5 @@
 # setwd('D:/DO/eDO_datathon')
-
+data_folder <- 'data/input/'
 # LIBRARIES --------------------------------------------------------------------
 library(data.table)
 source('code/tools_R.R')
@@ -17,10 +17,22 @@ df_clus <- add_cat(df_clus, 'cluster', clus, 'n_clus')
 
 # MERGE ALL DATA STRUCTURES ----------------------------------------------------
 df <- merge(df_clus,df_lab)
-df$AccomodationId <- strsplit(df$pk,'_')[[1]][1]
-
+df[, c("AccomodationId", "PictureId") := tstrsplit(pk, "_", fixed=TRUE)]
+df$AccomodationId <- as.numeric(df$AccomodationId)
 # GROUP BY ACCOMODATION --------------------------------------------------------
 
 # Get all features we want to summarize per column
+# select columns for h20
+
+select_col <- c('1','2','interiordesign','room','floor','suite','bedroom')
+df[, lapply(.SD, sum, na.rm = T), by = AccomodationId, .SDcols = select_col]
 
 
+# MERGE WITH TARGET ------------------------------------------------------------
+target <- fread(paste0(data_folder,'labels_targets/hotels_sample.csv'))
+
+df_h20 <- merge(target,df[,mget(c('AccomodationId', select_col))], 
+      by.x = 'ID', by.y = 'AccomodationId')
+
+save(list='df_h20', 
+     file = paste0(data_folder, 'generated_by_us/features/features.Rdata'))
--- code/05_automl_h20_snippet.R
+++ code/05_automl_h20_snippet.R
@@ -1,52 +1,41 @@
-library(digorig)
+# setwd('D:/DO/eDO_datathon')
+data_folder <- 'data/input/'
+# LIBRARIES --------------------------------------------------------------------
 library(h2o)
-do.init()
 # You can check the H2O UI on http://localhost:54321/flow/index.html
 h2o.init()
 
-# Import a sample binary outcome train/test set into H2O
-data = fread("data/output/df_features.csv")
+# LOAD DATA --------------------------------------------------------------------
+# Import a features dataset 
+load(paste0(data_folder, 'generated_by_us/features/features.Rdata'))
+# Transform the set to train/test set into H2O
+train  <- as.h2o(df_h20)
+test <- as.h2o(df_h20)
 
-df_ini = data %>% mutate(date_begin = lubridate::date(date_begin)) %>% filter(date_begin <= '2014-12-31' & !is.na(target))
-df_ini %>% View
-h2o.ini  <- as.h2o(df_ini)
-
-
-# Identify predictors and response
-
-disregard = c("target_Concentration", "target", "date_begin", "week_day")
-y <- "target"
-x <- setdiff(names(df_ini), disregard)
+# Identify predictors and response ---------------------------------------------
+y <- "TYPE"
+x <- setdiff(names(train), y)
 
 # For binary classification, response should be a factor
-h2o.ini[,y] <- as.factor(h2o.ini[,y])
-h2o.ini[,"station"] <- as.factor(h2o.ini[,"station"])
+train[,y] <- as.factor(train[,y])
+test[,y] <- as.factor(test[,y])
 
-
-aml <- h2o.automl(
-  x = x, 
-  y = y,
-  training_frame = h2o.ini,
-  max_runtime_secs = 30,
-  stopping_metric = "logloss"
-)
+aml <- h2o.automl(x = x, y = y,
+                  training_frame = train,
+                  max_runtime_secs = 30)
 
 # View the AutoML Leaderboard
 lb <- aml@leaderboard
 lb
 
-
 # The leader model is stored here
 aml@leader
-
 
 # If you need to generate predictions on a test set, you can make
 # predictions directly on the `"H2OAutoML"` object, or on the leader
 # model object directly
 
-#pred <- h2o.predict(aml, test)  #Not functional yet: https://0xdata.atlassian.net/browse/PUBDEV-4428
+pred <- h2o.predict(aml, test)  # predict(aml, test) also works
 
 # or:
 pred <- h2o.predict(aml@leader, test)
-
-
