dbe284c1c79a9501d59457dd8392d870cf630edf
Patricia Marquez <pmarquez@digitialorigin.com>
2018-10-20 13:41:45 +0200

    added to train automl with only labels

--- code/05_automl_h20_snippet.R
+++ code/05_automl_h20_snippet.R
@@ -1,16 +1,45 @@
 # setwd('D:/DO/eDO_datathon')
 data_folder <- 'data/input/'
 # LIBRARIES --------------------------------------------------------------------
+library(data.table)
 library(h2o)
 # You can check the H2O UI on http://localhost:54321/flow/index.html
 h2o.init()
 
 # LOAD DATA --------------------------------------------------------------------
-# Import a features dataset 
-load(paste0(data_folder, 'generated_by_us/features/features.Rdata'))
+# # Import a features dataset 
+# load(paste0(data_folder, 'generated_by_us/features/features.Rdata'))
+# 
+# # Transform the set to train/test set into H2O
+# train  <- as.h2o(df_h20)
+# test <- as.h2o(df_h20)
+
+# LOAD DATA WORDS --------------------------------------------------------------
+df <- fread(paste0(data_folder,
+                   'generated_by_us/labels_per_image/words_by_accomodation_train.csv'),
+            encoding = "UTF-8")
+
+# Removed failed labels
+df_clean <- df[, mget(names(df)[! grepl('Failed',names(df))])]
+# Remove all columns with a duplicated label
+# df_clean <- df_clean[, mget(names(df_clean)[!duplicated(names(df_clean))])]
+
+# join with type and also split train validation
+# read targets
+df_tg <- fread(paste0(data_folder,'labels_targets/accomodations_train_split.csv'),
+               encoding = "UTF-8")
+
+df_split <- merge(df_clean, df_tg, by.x = 'AccomodationId', by.y = 'ID')
+
+df_split <- df_split[, mget(names(df_split)[! grepl('V1',names(df_split))])]
+
+train <- df_split[is_test == 0,]
+test <- df_split[is_test == 1,]
+
 # Transform the set to train/test set into H2O
-train  <- as.h2o(df_h20)
-test <- as.h2o(df_h20)
+train  <- as.h2o(train)
+test <- as.h2o(test)
+
 
 # Identify predictors and response ---------------------------------------------
 y <- "TYPE"
